4b387c5a4929ca1efa0ffff371054244640ccefc
Luka <luka.bartula@stu.ibu.edu.ba>
2025-01-12 18:24:26 +0100

    final commit

--- src/test/java/SetupAndTearDown.java
+++ src/test/java/SetupAndTearDown.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SetupAndTearDown {
 
